83e0165bfe56de1bb8ab7bd2bd5bbb0d3a4dd8fd
LegionFaro <ggg>
2024-11-23 13:58:13 +0100

    home

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<?php
+if (function_exists('mysqli_connect')) {
+    echo "MySQLi extension is enabled!";
+} else {
+    echo "MySQLi extension is NOT enabled.";
+    phpinfo();
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
